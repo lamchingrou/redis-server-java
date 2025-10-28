2053a13ad313af35739bb8e0a819a6be21a43209
lamchingrou <72553981+lamchingrou@users.noreply.github.com>
2025-10-29 01:29:36 +0800

    fix import stmt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.OutputStream;
+import java.io.OutputStream;
 
 public class Main {
   public static void main(String[] args){
