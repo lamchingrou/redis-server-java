64cc036886c908d9c4bd6d8423d109870329eab3
lamchingrou <72553981+lamchingrou@users.noreply.github.com>
2025-11-04 21:48:43 +0800

    break after flush

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,12 @@ import java.io.InputStreamReader;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    //  Uncomment the code below to pass the first stage
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
       clientSocket = serverSocket.accept();
       System.out.println("Client connected!");
       
@@ -31,6 +24,7 @@ public class Main {
       while (reader.readLine() != null) { 
         out.write("+PONG\r\n".getBytes());
         out.flush();
+        break;
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
