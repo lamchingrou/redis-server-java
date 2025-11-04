ea04f751f3bd5402e9bb74cb5a59573004374d28
lamchingrou <72553981+lamchingrou@users.noreply.github.com>
2025-11-04 22:16:26 +0800

    correct answer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class Main {
   public static void main(String[] args){
@@ -14,18 +10,16 @@ public class Main {
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      System.out.println("Client connected!");
-      
-      OutputStream out = clientSocket.getOutputStream();
-      InputStream in = clientSocket.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-      
-      String line;
-      while ((line = reader.readLine()) != null) { 
-        System.out.println("Reveived: " + line);
-        out.write("+PONG\r\n".getBytes());
-        out.flush();
+
+      while (true) {
+        // Read input from client.
+        byte[] input = new byte[1024];
+        clientSocket.getInputStream().read(input);
+        String inputString = new String(input).trim();
+        System.out.println("Received: " + inputString);
+        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
